162fc489bcafe7a0f5a1f0808598d8cb17f4b093
Luiz Alberto Jr. <64710056+luizjxcoder@users.noreply.github.com>
2025-10-31 10:08:19 -0300

    build atualizado

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App.tsx"
+import "./index.css"
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Detecta se está em produção (GitHub Pages) e aplica o basename dinamicamente
+const isProd = window.location.hostname.includes("github.io")
+const baseName = isProd ? "/procoder-tasks" : "/"
+
+createRoot(document.getElementById("root")!).render(
+     <BrowserRouter basename={baseName}>
+          <App />
+     </BrowserRouter>
+)
+
